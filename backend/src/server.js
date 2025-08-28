f3bf1b0a7710844fb00a2fcca147855db464d829
Thomas De Oliveira <thomas.deoliveira.pro@outlook.fr>
2025-08-28 10:12:22 +0200

    🐛 Update server to listen on all network interfaces

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 // Synchronisation Sequelize puis lancement du serveur
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
   });
 });
